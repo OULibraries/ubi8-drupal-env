f084010c0a181b46976e545a1454f4c91d88d72b
Logan Cox <lo5an@users.noreply.github.com>
2020-04-28 18:55:33 -0400

    Update Drush version and add Drupal Console.

--- bin/drp_create_site.sh
+++ bin/drp_create_site.sh
@@ -11,22 +11,18 @@ USAGE
   exit 1;
 fi
 
-
 if [ ! -d "./sites" ]; then
   echo "No sites directory. Are you in the right place?"
-
   exit 1;
-
 fi
 
-
-
-
-## Grab the base for SITEPATH and MASTERPATH to use as slugs
 SITE=$1
 
-## Sanitize the DB slug by excluding everything that MySQL doesn't like from $SITE
-DBSLUG=$(echo -n  "${SITE}" | tr -C '_A-Za-z0-9' '_')
+if [ -d "./sites/${SITE}" ]; then
+  echo "There's already a something at ./sites/${SITE}."
+  exit 1;
+fi
+
 
 ## 
 echo "Let's build a site!"
@@ -38,7 +34,11 @@ mkdir -p "./sites/${SITE}"
 US=$(id -u)
 GP=$(id -g)
 
-docker run --env COMPOSER_HOME=/tmp/composer --user ${US}:${GP} --volume "$PWD":/mnt/data:z --workdir /mnt/data --rm -it drp-cli composer create-project drupal/recommended-project ./sites/${SITE} && composer require --working-dir ./sites/${SITE} drush/drush
+docker run --env COMPOSER_HOME=/tmp/composer --user ${US}:${GP} --volume "$PWD":/mnt/data:z --workdir /mnt/data --rm -it drp-cli bash -c \
+"composer create-project drupal/recommended-project ./sites/${SITE} \
+&& composer require --working-dir ./sites/${SITE} drush/drush:~10  drupal/console:~1 \
+  --prefer-dist \
+  --optimize-autoloader"
 
 read -r -d '' INCLUDELOCAL <<- EOF
 if (file_exists(\$app_root . '/' . \$site_path . '/settings.local.php')) {
@@ -46,8 +46,8 @@ if (file_exists(\$app_root . '/' . \$site_path . '/settings.local.php')) {
 }
 EOF
 
-cp ./sites/${SITE}/web/sites/default/default.settings.php ./sites/${SITE}/web/sites/default/settings.php
-echo  ${INCLUDELOCAL} >> ./sites/${SITE}/web/sites/default/settings.php
+cp "./sites/${SITE}/web/sites/default/default.settings.php" "./sites/${SITE}/web/sites/default/settings.php"
+echo  "${INCLUDELOCAL}" >> "./sites/${SITE}/web/sites/default/settings.php"
 
 echo
 echo "Don't forget to add this site to git!"
